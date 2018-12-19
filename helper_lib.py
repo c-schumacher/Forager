c453b06e8339e297851d0c4af4a8ed094756e3bb
Charlie Schumacher <45952318+c-schumacher@users.noreply.github.com>
2018-12-19 15:35:49 -0600

    Update helper_lib.py

--- helper_lib.py
+++ helper_lib.py
@@ -6,13 +6,11 @@ from matplotlib import patches, figure
 import collections
 import time
 
-
 def frange(start, stop, inc):
     while start <= stop:
         yield start
         start += inc
 
-        
 def bootstrap(x, confidence = 0.68, nsamples = 100):
     def mean(S): 
         return float(sum(x for x in S))/len(S)
@@ -22,14 +20,11 @@ def bootstrap(x, confidence = 0.68, nsamples = 100):
     right_tail = nsamples-1-left_tail    
     return means[left_tail], mean(x), means[right_tail]
 
-
 def resample(S,size=None):
     return [random.choice(S) for i in xrange(size or len(S))]
 
-
 def E(f,S): 
     return float(sum(f(x) for x in S))/(len(S) or 1)
-
 
 def mean(X): 
     return E(lambda x:x, X)
