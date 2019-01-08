0140a4bcc1cf3cd723fc907e8b8425be83cacf80
Charlie Schumacher <45952318+c-schumacher@users.noreply.github.com>
2019-01-08 16:01:27 -0600

    Update lognorm_walker.py

--- lognorm_walker.py
+++ lognorm_walker.py
@@ -1,6 +1,6 @@
 class Lognorm_Walker(Forager):
-    def __init__(self, mu, sigma, agarRadius=75.0, foodRadius=5.0, resourceCount=5):
-        Forager.__init__(self, agarRadius, foodRadius, resourceCount)
+    def __init__(self, mu, sigma, envRadius=75.0, resoRadius=5.0, resoCount=5):
+        Forager.__init__(self, envRadius, resoRadius, resoCount)
         self.mu = mu
         self.sigma = sigma
         
