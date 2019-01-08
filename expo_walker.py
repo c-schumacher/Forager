863d63cc4e00c6fcab2560cd9e878c214f9fc9bf
Charlie Schumacher <45952318+c-schumacher@users.noreply.github.com>
2019-01-08 16:01:16 -0600

    Update expo_walker.py

--- expo_walker.py
+++ expo_walker.py
@@ -1,6 +1,6 @@
 class Expo_Walker(Forager):
-    def __init__(self, lambd, agarRadius=75.0, foodRadius=5.0, resourceCount=5):
-        Forager.__init__(self, agarRadius, foodRadius, resourceCount)
+    def __init__(self, lambd, envRadius=75.0, resoRadius=5.0, resoCount=5):
+        Forager.__init__(self, envRadius, resoRadius, resoCount)
         self.lambd = lambd
         
     def distRNG(self):
