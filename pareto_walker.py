5f8513b696b60325d708d53086892b94f3599dfa
Charlie Schumacher <45952318+c-schumacher@users.noreply.github.com>
2019-01-08 16:01:03 -0600

    Update pareto_walker.py

--- pareto_walker.py
+++ pareto_walker.py
@@ -1,6 +1,6 @@
 class Pareto_Walker(Forager):
-    def __init__(self, alpha, xmin=1.0, agarRadius=75.0, foodRadius=5.0, resourceCount=5):
-        Forager.__init__(self, agarRadius, foodRadius, resourceCount)
+    def __init__(self, alpha, xmin=1.0, envRadius=75.0, resoRadius=5.0, resoCount=5):
+        Forager.__init__(self, envRadius, resoRadius, resoCount)
         self.alpha = alpha
         self.xmin = xmin
         
